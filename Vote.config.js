ac70f546ad7ca40c8310d36e16c42c17a9868278
Bryan Savage <sav79tech@gmail.com>
2026-03-12 10:10:56 -0700

    Add Vite configuration for development and build

---
+++ Vote.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  server: {
+    host: true,
+    port: 5173
+  },
+
+  build: {
+    target: "esnext",
+    outDir: "dist"
+  },
+
+  define: {
+    global: "globalThis"
+  }
+});
